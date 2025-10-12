a329ca0e86fa6f3f916d6e1bf9f5f7a4be03277d
jaskaran-Rpay <jaskaran.singh@razorpay.com>
2025-10-12 20:59:59 +0530

    added comments

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,6 +5,8 @@ import (
 	"github.com/jaskaran-Rpay/go-bookstore/pkg/controllers"
 )
 
+// added comment
+
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
