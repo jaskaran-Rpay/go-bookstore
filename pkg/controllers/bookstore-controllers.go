90295d8829f9fc4b5afa2e4e8f2abc399f4f37e1
ohh-no <singh.jaskaran2024@gmail.com>
2025-10-15 11:23:35 +0530

    Made changes to jrp profile

--- pkg/controllers/bookstore-controllers.go
+++ pkg/controllers/bookstore-controllers.go
@@ -24,6 +24,7 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 func GetBookById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fmt.Println("vars", vars)
+	fmt.Print("Bye")
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
